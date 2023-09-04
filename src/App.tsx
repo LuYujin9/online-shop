7804f686a2df931f3f65f2fe62603a2227230b18
LuYujin9 <wuwujane@hotmail.com>
2023-09-04 22:57:24 +0200

    tried to put some functions and variables of UserAccount in App.tsx. but doesn't work, because I can't find the way to transfer the props with React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 //import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import ShoppingCart from "./pages/ShoppingCart";
 import Favorite from "./pages/Favorite";
 import UserAccount from "./pages/UserAccount";
+import { users } from "../public/data";
+import { User } from "./components/global.type";
+
+type users = User[];
+type newUserData = {
+  [k: string]: FormDataEntryValue;
+};
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isShowMessage, setIsShowMessage] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  const hangdleLogin = (newUserData: newUserData) => {
+    const user = users.find(
+      (user) =>
+        user.name == newUserData.userName &&
+        user.password == newUserData.password
+    );
+
+    if (user) {
+      setUser(user);
+      setIsLoggedIn(true);
+      setIsShowMessage(false);
+    } else {
+      setIsShowMessage(true);
+    }
+  };
+
+  const hangdleLogout = () => {
+    setUser(null);
+    setIsLoggedIn(false);
+    setIsShowMessage(false);
+  };
+
   return (
     <>
       <Header />
@@ -15,9 +49,23 @@ function App() {
         <Route path="/" Component={Homepage} />
         <Route path="/shopping-cart" Component={ShoppingCart} />
         <Route path="/favorite" Component={Favorite} />
-        <Route path="/user" Component={UserAccount} />
+        <Route path="/user" render={(routeProps) => (<UserAccount {...routeProps}  user={user}
+  isLoggedIn={isLoggedIn}
+  isShowMessage={isShowMessage}
+  onLogin={hangdleLogin}
+  onLogout={hangdleLogout}/>))/>
       </Routes>
     </>
   );
 }
 export default App;
+
+{/* <Route path="/user">
+<UserAccount
+  user={user}
+  isLoggedIn={isLoggedIn}
+  isShowMessage={isShowMessage}
+  onLogin={hangdleLogin}
+  onLogout={hangdleLogout}
+/>
+</Route> */}
