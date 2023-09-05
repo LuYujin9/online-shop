30754a6b2cba0f56cd8b2bd979235c89afffc60e
LuYujin9 <wuwujane@hotmail.com>
2023-09-05 20:55:19 +0200

    created the component BookmarkButton

---
+++ src/components/BookmarkButton.tsx
@@ -0,0 +1,23 @@
+import { FiStar } from "react-icons/fi";
+
+type BookmarkButtonProps = {
+  isFavorite: boolean;
+  toggleFavorite: () => void;
+};
+
+const BookmarkButton = ({
+  isFavorite,
+  toggleFavorite,
+}: BookmarkButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={() => toggleFavorite()}
+      className={isFavorite ? "aktive-favorite-button" : "favorite-button"}
+    >
+      <FiStar />
+    </button>
+  );
+};
+
+export default BookmarkButton;
