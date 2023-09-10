8bcc7e5763275ba16cd8d146e3656cd52c058049
LuYujin9 <wuwujane@hotmail.com>
2023-09-10 14:49:45 +0200

    added the component Cartbadge

---
+++ src/components/Cartbadge.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+type CartBadgeProps = {
+  itemCount: number;
+};
+
+const CartBadge = ({ itemCount }: CartBadgeProps) => {
+  return (
+    <div>
+      {itemCount > 0 && <Badge>{itemCount > 9 ? "9+" : itemCount}</Badge>}
+    </div>
+  );
+};
+export default CartBadge;
+
+const Badge = styled.div`
+  width: 1em;
+  height: 1em;
+  border-radius: 0.5em;
+  font-size: 15px;
+  color: white;
+  background-color: red;
+  text-align: center;
+
+  position: relative;
+  right: 5px;
+  top: 10px;
+`;
