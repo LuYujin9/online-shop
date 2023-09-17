ddd5414c66807c0c9d8a649c3cf6e88662a4cc46
LuYujin9 <wuwujane@hotmail.com>
2023-09-17 21:31:38 +0200

    changed the title of this online shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useLocalStorage } from "usehooks-ts";
